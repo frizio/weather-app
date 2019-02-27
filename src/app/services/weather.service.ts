8abaa6e67f71b4204440bd7f026368b386b8691c
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-02-27 11:17:14 +0100

    Get data with a service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  APIVERSION = '2.5';
+  APIKEY = '4442c84151025f9500ba16a03da3dc68';
+  URI: string;
+
+  constructor(
+    private httpClient: HttpClient
+  ) {
+    this.URI = `https://api.openweathermap.org/data/${this.APIVERSION}/weather?appid=${this.APIKEY}`;
+  }
+
+  getWeather(cityName: string, countryCode: string) {
+    return this.httpClient.get(`${this.URI}&${cityName},${countryCode}`);
+  }
+
 }
